39def1549b1790036a8cf7ff849018318b7d31b4
208418037 <208418037@gms.tku.edu.tw>
2021-09-30 20:03:01 +0800

    first commit

---
+++ src/App_37.js
@@ -0,0 +1,15 @@
+import Todo_37 from './components/Todo_37';
+import './App.css';
+
+function App_37(props) {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo_37 text="Learn A" />
+      <Todo_37 text="Learn B" />
+      <Todo_37 text="Learn C" />
+    </div>
+  );
+}
+
+export default App_37;
